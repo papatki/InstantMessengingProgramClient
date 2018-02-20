e4d7ecd08fc860a37e5f361505ba42998d60108e
Patrycja <patrycja.p9428@gmail.com>
2018-02-20 15:03:53 +0100

    Update ClientMain.java

--- src/main/java/com/patrycjap/ClientMain.java
+++ src/main/java/com/patrycjap/ClientMain.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class ClientMain {
     public static void main(String[] args) {
-        Client client = new Client("127.0.0.1"); //local host - computer there I am right now
+        Client client = new Client("127.0.0.1");
         client.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         client.startRunning();
 
